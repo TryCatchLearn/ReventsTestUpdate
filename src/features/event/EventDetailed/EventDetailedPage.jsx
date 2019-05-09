15c3a5a281a927df082c4599efb620c7152f0e84
trycatchlearn <trycatchlearn@outlook.com>
2019-05-09 07:53:44 +0700

    End of section 6

---
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EventDetailedPage = () => {
+  return (
+    <div>
+      <h1>Event Detailed Page</h1>
+    </div>
+  )
+}
+
+export default EventDetailedPage
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
@@ -7,13 +8,18 @@ import * as serviceWorker from './serviceWorker';
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(<App/>, rootEl)
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    })
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
 render();
