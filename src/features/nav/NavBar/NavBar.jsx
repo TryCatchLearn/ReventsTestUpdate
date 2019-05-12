ac596a3ce4e8b1b3629ee7c7e50110dd61b0579a
trycatchlearn <trycatchlearn@outlook.com>
2019-05-12 21:27:24 +0700

    End of section 18

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -60,6 +60,7 @@ class NavBar extends Component {
 
           {authenticated ? (
             <SignedInMenu
+              auth={auth}
               profile={profile}
               signOut={this.handleSignOut}
             />
