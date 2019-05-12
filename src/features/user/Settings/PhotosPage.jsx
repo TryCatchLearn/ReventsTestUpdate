b97b528946631958c4d55aa0b4dd73be2836e602
trycatchlearn <trycatchlearn@outlook.com>
2019-05-12 20:11:27 +0700

    End of section 17

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,11 +1,227 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import {
+  Image,
+  Segment,
+  Header,
+  Divider,
+  Grid,
+  Button,
+  Card,
+  Icon
+} from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+import { uploadProfileImage, deletePhoto, setMainPhoto } from '../userActions';
+import { toastr } from 'react-redux-toastr';
 
-const PhotosPage = () => {
-  return (
-    <div>
-      <h1>Photos page</h1>
-    </div>
-  )
+const query = ({ auth }) => {
+  return [
+    {
+      collection: 'users',
+      doc: auth.uid,
+      subcollections: [{ collection: 'photos' }],
+      storeAs: 'photos'
+    }
+  ];
+};
+
+const actions = {
+  uploadProfileImage,
+  deletePhoto,
+  setMainPhoto
+};
+
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos,
+  loading: state.async.loading
+});
+
+class PhotosPage extends Component {
+  state = {
+    files: [],
+    fileName: '',
+    preview: null,
+    cropResult: null,
+    image: {}
+  };
+
+  uploadimage = async () => {
+    try {
+      await this.props.uploadProfileImage(
+        this.state.image,
+        this.state.fileName
+      );
+      this.cancelCrop();
+      toastr.success('Success', 'Photo has been uploaded');
+    } catch (error) {
+      toastr.error('Oops', error.message);
+    }
+  };
+
+  handlePhotoDelete = (photo) => async () => {
+    try  {
+      this.props.deletePhoto(photo);
+    } catch (error) {
+      toastr.error('Oops', error.message)
+    }
+  }
+
+  handleSetMainPhoto = (photo) => async () => {
+    try {
+      this.props.setMainPhoto(photo);
+    } catch (error) {
+      toastr.error('Oops', error.message);
+    }
+  }
+
+  cancelCrop = () => {
+    this.setState({
+      files: [],
+      image: {},
+      preview: null
+    });
+  };
+
+  onDrop = files => {
+    this.setState({
+      files,
+      fileName: files[0].name,
+      preview: URL.createObjectURL(files[0])
+    });
+  };
+
+  cropImage = () => {
+    if (typeof this.refs.cropper.getCroppedCanvas() === 'undefined') {
+      return;
+    }
+
+    this.refs.cropper.getCroppedCanvas().toBlob(blob => {
+      let imageUrl = URL.createObjectURL(blob);
+      this.setState({
+        cropResult: imageUrl,
+        image: blob
+      });
+    }, 'image/jpeg');
+  };
+
+  render() {
+    const { photos, profile, loading } = this.props;
+    let filteredPhotos;
+    if (photos) {
+      filteredPhotos = photos.filter(photo => {
+        return photo.url !== profile.photoURL
+      })
+    }
+    return (
+      <Segment>
+        <Header dividing size='large' content='Your Photos' />
+        <Grid>
+          <Grid.Row />
+          <Grid.Column width={4}>
+            <Header color='teal' sub content='Step 1 - Add Photo' />
+            <Dropzone
+              onDrop={acceptedFiles => this.onDrop(acceptedFiles)}
+              multiple={false}
+            >
+              {({ getRootProps, getInputProps, isDragActive }) => (
+                <div
+                  {...getRootProps()}
+                  className={
+                    'dropzone ' + (isDragActive && 'dropzone--isActive')
+                  }
+                >
+                  <input {...getInputProps()} />
+                  <Icon name='upload' size='huge' />
+                  <Header content='Drop image here' />
+                </div>
+              )}
+            </Dropzone>
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color='teal' content='Step 2 - Resize image' />
+            {this.state.preview && (
+              <Cropper
+                style={{ height: 200, width: '100%' }}
+                ref='cropper'
+                src={this.state.preview}
+                aspectRatio={1}
+                viewMode={0}
+                dragMode='move'
+                guides={false}
+                scalable={true}
+                cropBoxMovable={true}
+                cropBoxResizable={true}
+                crop={this.cropImage}
+              />
+            )}
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color='teal' content='Step 3 - Preview and Upload' />
+            {this.state.preview && (
+              <Fragment>
+                <Image
+                  style={{ minHeight: '200px', minWidth: '200px' }}
+                  src={this.state.cropResult}
+                />
+                <Button.Group>
+                  <Button
+                    loading={loading}
+                    onClick={this.uploadimage}
+                    style={{ width: 100 }}
+                    positive
+                    icon='check'
+                  />
+                  <Button
+                    disabled={loading}
+                    onClick={this.cancelCrop}
+                    style={{ width: 100 }}
+                    icon='close'
+                  />
+                </Button.Group>
+              </Fragment>
+            )}
+          </Grid.Column>
+        </Grid>
+
+        <Divider />
+        <Header sub color='teal' content='All Photos' />
+
+        <Card.Group itemsPerRow={5}>
+          <Card>
+            <Image src={profile.photoURL || '/assets/user.png'} />
+            <Button positive>Main Photo</Button>
+          </Card>
+          {photos &&
+            filteredPhotos.map(photo => (
+              <Card key={photo.id}>
+                <Image src={photo.url} />
+                <div className='ui two buttons'>
+                  <Button onClick={this.handleSetMainPhoto(photo)} basic color='green'>
+                    Main
+                  </Button>
+                  <Button onClick={this.handlePhotoDelete(photo)} basic icon='trash' color='red' />
+                </div>
+              </Card>
+            ))}
+        </Card.Group>
+
+      </Segment>
+    );
+  }
 }
 
-export default PhotosPage
+export default compose(
+  connect(
+    mapState,
+    actions
+  ),
+  firestoreConnect(auth => query(auth))
+)(PhotosPage);
